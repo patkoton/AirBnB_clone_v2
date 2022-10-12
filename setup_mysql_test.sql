7860ae0ef85788025d7732753740e6fe90c15a93
patkoton <68227675+patkoton@users.noreply.github.com>
2022-10-12 12:02:22 +0100

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,7 @@
--- prepares a MySQL test server for the AirBnb clone
--- creates a database
+-- Script to create a MySQL server with the database hbnb_test_db.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
--- grant SELECT privileges
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
